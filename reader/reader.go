e2683db4b97a0cd759a911e1608f1f83d4cae9df
mario <mario.briseno7@gmail.com>
2021-03-07 13:03:33 -0600

    removing fmt statements

--- reader/reader.go
+++ reader/reader.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
-	"math/big"
 	"net"
 	"os"
 )
 
-type Event struct {
-	Address string;
-	Reserve0 *big.Int;
-	Reserve1 *big.Int;
-}
 
 func main() {
 
@@ -32,13 +25,9 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	event := Event{}
 	for {
-		fmt.Println("reading answer")
 		var buff [1024]byte
 		n, err := conn.Read(buff[:])
-		abc := json.Unmarshal(buff[:n], &event)
-		fmt.Println(abc)
 		fmt.Printf("%s\n", string(buff[:n]));
 		if err != nil {
 			log.Fatal(err)
